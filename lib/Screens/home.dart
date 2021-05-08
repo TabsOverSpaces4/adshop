ca0ef776ee039d735a962fd775a529dd78948852
TabsOverSpaces4 <guptaharsh.2401@gmail.com>
2021-05-08 19:47:35 +0530

    Functional btmNavBar

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -9,20 +9,56 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  PageController _tabsPageController;
+  int _selectedTab = 0;
+
+  @override
+  void initState() {
+    _tabsPageController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabsPageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            child: Center(
-              child: Text("Homepage"),
-            ),
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+            child: Expanded(
+          child: PageView(
+            controller: _tabsPageController,
+            onPageChanged: (num) {
+              setState(() {
+                _selectedTab = num;
+              });
+            },
+            children: [
+              Container(
+                child: Center(child: Text("Homepage")),
+              ),
+              Container(
+                child: Center(child: Text("Search Page")),
+              ),
+              Container(
+                child: Center(child: Text("Saved Page")),
+              ),
+              Container(
+                child: Center(child: Text("Profile Page")),
+              )
+            ],
           ),
-         Btmtabs(),
-        ],
-      )
-    );
+        )),
+        Btmtabs(
+          selectedTab: _selectedTab,
+        ),
+      ],
+    ));
   }
 }
--- lib/Widgets/bottomtabs.dart
+++ lib/Widgets/bottomtabs.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
-class Btmtabs extends StatelessWidget {
+class Btmtabs extends StatefulWidget {
+  final int selectedTab;
+  Btmtabs({this.selectedTab});
 
+  @override
+  _BtmtabsState createState() => _BtmtabsState();
+}
+
+class _BtmtabsState extends State<Btmtabs> {
+  int _selectedTab = 0;
 
 
   @override
   Widget build(BuildContext context) {
+    _selectedTab = widget.selectedTab ?? 0;
     return Container(
         decoration: BoxDecoration(
             color: Colors.white,
@@ -25,16 +34,39 @@ class Btmtabs extends StatelessWidget {
           children: [
             BtmtabBtn(
               imagePath: "lib/Assets/images/tab_home.png",
-              selected: true,
+              selected: _selectedTab == 0 ? true : false,
+              onPressed: () {
+                setState(() {
+                  _selectedTab = 0;
+                });
+              },
             ),
             BtmtabBtn(
               imagePath: "lib/Assets/images/tab_search.png",
+              selected: _selectedTab == 1 ? true : false,
+              onPressed: () {
+                setState(() {
+                  _selectedTab = 1;
+                });
+              },
             ),
             BtmtabBtn(
               imagePath: "lib/Assets/images/tab_saved.png",
+              selected: _selectedTab == 2 ? true : false,
+              onPressed: () {
+                setState(() {
+                  _selectedTab = 2;
+                });
+              },
             ),
             BtmtabBtn(
               imagePath: "lib/Assets/images/tab_logout.png",
+              selected: _selectedTab == 3 ? true : false,
+              onPressed: () {
+                setState(() {
+                  _selectedTab = 3;
+                });
+              },
             ),
           ],
         ));
@@ -44,30 +76,32 @@ class Btmtabs extends StatelessWidget {
 class BtmtabBtn extends StatelessWidget {
   final String imagePath;
   final bool selected;
-  BtmtabBtn({this.imagePath, this.selected});
+  final Function onPressed;
+  BtmtabBtn({this.imagePath, this.selected, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     bool _selected = selected ?? false;
 
-    return Container(
-      padding: EdgeInsets.symmetric(
-        vertical: 28.0,
-        horizontal: 24.0,
-      ),
-      decoration: BoxDecoration(
-        border: Border(
-          top: BorderSide(
-            color: _selected ? Theme.of(context).accentColor : Colors.transparent,
-            width: 2.0,
-          )
-        )
-      ),
-      child: Image(
-        image: AssetImage(imagePath ?? "lib/Assets/images/tab_home.png"),
-        width: 26.0,
-        height: 26.0,
-        color: _selected ? Theme.of(context).accentColor : Colors.black,
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 28.0,
+          horizontal: 24.0,
+        ),
+        decoration: BoxDecoration(
+            border: Border(
+                top: BorderSide(
+          color: _selected ? Theme.of(context).accentColor : Colors.transparent,
+          width: 2.0,
+        ))),
+        child: Image(
+          image: AssetImage(imagePath ?? "lib/Assets/images/tab_home.png"),
+          width: 26.0,
+          height: 26.0,
+          color: _selected ? Theme.of(context).accentColor : Colors.black,
+        ),
       ),
     );
   }
