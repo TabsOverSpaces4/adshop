c460e7ca0827529f0a476d77614d397f941883fe
TabsOverSpaces4 <guptaharsh.2401@gmail.com>
2021-05-09 22:31:43 +0530

    Network image problem

--- lib/tabs/hometab.dart
+++ lib/tabs/hometab.dart
@@ -1,20 +1,51 @@
 import 'package:adshop/Widgets/custom_action_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
+  final CollectionReference _productsRef =
+      FirebaseFirestore.instance.collection("Products");
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Stack(
         children: [
-          Center(child: Text("Home Tab"),),
+          FutureBuilder<QuerySnapshot>(
+            future: _productsRef.get(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Scaffold(
+                  body: Center(
+                    child: Text("Error: ${snapshot.error}"),
+                  ),
+                );
+              }
+
+              //Collected data is ready to be displayed
+              if (snapshot.connectionState == ConnectionState.done) {
+                return ListView(
+                  children: snapshot.data.docs.map((document) {
+                    return Container(
+                        child: Image.network("${document.data()}"));
+                  }).toList(),
+                );
+              }
+
+              //loading state
+              return Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            },
+          ),
           CustomActionBar(
             hasBackArrow: false,
             title: "Home",
           ),
         ],
       ),
-      
     );
   }
 }
