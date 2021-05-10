473bec59d8206be949fe0a7717cc72ced820735f
TabsOverSpaces4 <guptaharsh.2401@gmail.com>
2021-05-10 20:11:22 +0530

    Animated swiper

--- lib/Widgets/image_swiper.dart
+++ lib/Widgets/image_swiper.dart
@@ -9,19 +9,53 @@ class ImageSwipe extends StatefulWidget {
 }
 
 class _ImageSwipeState extends State<ImageSwipe> {
+  int _selectedPage = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 400.0,
-      child: PageView(
+      child: Stack(
         children: [
-          for (var i = 0; i < widget.imageList.length; i++)
-            Container(
-              child: Image.network(
-                "${widget.imageList[i]}",
-                fit: BoxFit.cover,
-              ),
-            )
+          PageView(
+            onPageChanged: (num) {
+              setState(() {
+                _selectedPage = num;
+              });
+            },
+            children: [
+              for (var i = 0; i < widget.imageList.length; i++)
+                Container(
+                  child: Image.network(
+                    "${widget.imageList[i]}",
+                    fit: BoxFit.cover,
+                  ),
+                )
+            ],
+          ),
+          Positioned(
+            bottom: 20.0,
+            left: 0.0,
+            right: 0.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var i = 0; i < widget.imageList.length; i++)
+                  AnimatedContainer(
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.easeOutCubic,
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 4.0,
+                    ),
+                    width: _selectedPage == i ? 35.0 : 12.0,
+                    height: 12.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.85),
+                        borderRadius: BorderRadius.circular(12.0)),
+                  )
+              ],
+            ),
+          )
         ],
       ),
     );
